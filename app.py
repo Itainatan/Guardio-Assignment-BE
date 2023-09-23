3f8dffbf64306f3a082654ef7730ef31d0f8192a
itai natan <itai08425@gmail.com>
2023-09-24 01:48:04 +0300

    updates for filter

--- app.py
+++ app.py
@@ -5,29 +5,35 @@ import db
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/icon/<name>')
-def get_icon_url(name:str):
+
+@app.route("/icon/<name>")
+def get_icon_url(name: str):
     return f"https://img.pokemondb.net/sprites/silver/normal/{name}.png"
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    page = int(request.args.get('page', 1))
-    page_size = int(request.args.get('pageSize', 50))
-    filter_by = request.args.get('filterBy', None)
-    sort = request.args.get('sort', None)
+    page = int(request.args.get("page", 1))
+    page_size = int(request.args.get("pageSize", 50))
+    filters = request.args.getlist("filters")
+    sort = request.args.get("sort", None)
 
     data = db.get()
 
+    filter_conditions = {}
+
     filtered_data = data
 
-    if filter_by:
-        filtered_data = [item for item in filtered_data if item.get('type_one', '').lower().startswith(filter_by.lower())]
+    for filter_str in filters:
+        filter_conditions = filter_str.split("&")
+        for condition in filter_conditions:
+            key, values = condition.split("=")
+            values = values.split(",")
+            filtered_data = [item for item in filtered_data if item.get(key).lower() in values]
 
     if sort:
-        reverse_sort = sort.lower() == 'descending'
-        filtered_data.sort(key=lambda item: item.get('number'), reverse=reverse_sort)
-
+        reverse_sort = sort.lower() == "descending"
+        filtered_data.sort(key=lambda item: item.get("number"), reverse=reverse_sort)
 
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
@@ -36,17 +42,13 @@ def hello():
     if page <= 0 or page_size <= 0:
         return "Invalid page or pageSize", 400
 
-    
     total_length = len(filtered_data)
     paginated_data = filtered_data[start_index:end_index]
 
-    response = {
-        'list': paginated_data,
-        'totalLength': total_length
-    }
-    
+    response = {"list": paginated_data, "totalLength": total_length}
+
     return jsonify(response)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run(port=8080)
